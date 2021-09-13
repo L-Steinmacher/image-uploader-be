a24bfca03bcf6121a734d4b835a5c5d45c972939
L-Steinmacher <dasai.sacul@gmail.com>
2021-09-13 10:49:01 -0700

    first commit

---
+++ src/main/java/com/example/imageuploaderapp/ImageUploaderAppApplication.java
@@ -0,0 +1,16 @@
+package com.example.imageuploaderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageUploaderAppApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ImageUploaderAppApplication.class,
+            args);
+    }
+
+}
