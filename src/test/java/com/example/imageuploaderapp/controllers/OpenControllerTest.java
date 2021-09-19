e3c6e5559497ff834633e2430c315c10fc2a149b
L-Steinmacher <dasai.sacul@gmail.com>
2021-09-19 11:06:15 -0700

    tests added

---
+++ src/test/java/com/example/imageuploaderapp/controllers/OpenControllerTest.java
@@ -0,0 +1,19 @@
+package com.example.imageuploaderapp.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpenControllerTest
+{
+
+    @Test
+    void addSelf()
+    {
+    }
+
+    @Test
+    void testAddSelf()
+    {
+    }
+}
