669d705447e97d2964055f6e730ede930b352da2
L-Steinmacher <dasai.sacul@gmail.com>
2021-09-19 11:10:42 -0700

    changes testing from userServices to User ServicesImpl

--- src/test/java/com/example/imageuploaderapp/services/UserServiceImplTest.java
+++ src/test/java/com/example/imageuploaderapp/services/UserServiceImplTest.java
@@ -1,8 +1,49 @@
 package com.example.imageuploaderapp.services;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceImplTest
 {
 
+    @Test
+    void findUserById()
+    {
+    }
+
+    @Test
+    void findByNameContaining()
+    {
+    }
+
+    @Test
+    void findAll()
+    {
+    }
+
+    @Test
+    void delete()
+    {
+    }
+
+    @Test
+    void findByName()
+    {
+    }
+
+    @Test
+    void deleteAll()
+    {
+    }
+
+    @Test
+    void save()
+    {
+    }
+
+    @Test
+    void update()
+    {
+    }
 }
