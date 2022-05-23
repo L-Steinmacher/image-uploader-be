5e064f414516a36e780eea94d8d4076b9f9945c7
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2022-05-23 16:27:19 -0700

    feature: created trail repository interface

--- src/main/java/com/example/imageuploaderapp/models/Trail.java
+++ src/main/java/com/example/imageuploaderapp/models/Trail.java
@@ -1,7 +1,10 @@
 package com.example.imageuploaderapp.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "trails")
@@ -11,11 +14,83 @@ public class Trail extends Auditable
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long trailid;
 
+    @Column(nullable = false)
     private String trailname;
 
-    private float averagerating;
+    @Column(nullable = false)
+    private String traildiscription;
 
-    private String discription;
+    @Column(nullable = false)
+    private double latitude;
 
+    @Column(nullable = false)
+    private double longitude;
 
+    @OneToMany(mappedBy = "trail",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    @JsonIgnoreProperties(value = "trail",
+            allowSetters = true)
+    private List<Hike> hikes = new ArrayList<>();
+
+    public Trail() {
+    }
+
+    public Trail(String trailname,
+                 String traildiscription,
+                 double latitude,
+                 double longitude) {
+        this.trailname = trailname;
+        this.traildiscription = traildiscription;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public long getTrailid() {
+        return trailid;
+    }
+
+    public void setTrailid(long trailid) {
+        this.trailid = trailid;
+    }
+
+    public String getTrailname() {
+        return trailname;
+    }
+
+    public void setTrailname(String trailname) {
+        this.trailname = trailname;
+    }
+
+    public String getTraildiscription() {
+        return traildiscription;
+    }
+
+    public void setTraildiscription(String traildiscription) {
+        this.traildiscription = traildiscription;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public List<Hike> getHikes() {
+        return hikes;
+    }
+
+    public void setHikes(List<Hike> hikes) {
+        this.hikes = hikes;
+    }
 }
---
+++ src/main/java/com/example/imageuploaderapp/repository/TrailRepository.java
@@ -0,0 +1,7 @@
+package com.example.imageuploaderapp.repository;
+
+import com.example.imageuploaderapp.models.Trail;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TrailRepository extends CrudRepository<Trail, Long> {
+}
